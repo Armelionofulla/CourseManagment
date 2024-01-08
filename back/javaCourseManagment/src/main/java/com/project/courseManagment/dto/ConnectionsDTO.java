3ba3838a247afb01d160f1d5b2180de099d97a4c
SaraMarketa <sara.marketa@fti.edu.al>
2024-01-08 22:48:16 +0100

    dto

---
+++ back/javaCourseManagment/src/main/java/com/project/courseManagment/dto/ConnectionsDTO.java
@@ -0,0 +1,18 @@
+package com.project.courseManagement.dto;
+
+public interface ConnectionsDTO {
+    String getStudentName();
+
+    String getLastName();
+
+    String getUsername();
+
+    String getEmail();
+
+    String getAddress();
+
+    String getPicture();
+
+    Long getId();
+
+}
