e3fa41357589d91de678cc782b3df5c819c31de7
SaraMarketa <sara.marketa@fti.edu.al>
2024-01-09 15:26:28 +0100

    Dto

---
+++ back/javaCourseManagment/src/main/java/com/project/courseManagment/dto/CommentWithStudentDTO.java
@@ -0,0 +1,14 @@
+package com.project.courseManagement.dto;
+
+public interface CommentWithStudentDTO {
+
+    Long getId();
+    String getLikes();
+
+    String getText();
+
+    Long getStudentId();
+
+    String getUsername();
+
+}
