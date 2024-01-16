dec6ff979a9fcb7a0ea4ecd5e6453837d6600221
SaraMarketa <sara.marketa@fti.edu.al>
2024-01-16 22:17:27 +0100

    Course Interfaces

---
+++ front/javaProjectFront/src/types/courses.ts
@@ -0,0 +1,50 @@
+import { LoginUserInput } from './user'
+export interface Course {
+  id: Number
+  name: string
+  startDate: string
+  endDate: string
+  type: string
+  image: string
+  lecture_hall: string
+  course_time: string
+}
+
+export interface Courses {
+  major: Course[]
+  minor: Course[]
+}
+
+export interface CourseRespone {
+  address: string
+  courseName: string
+  email: string
+  endDate: string
+  image: string
+  lastname: null
+  likes: number
+  studentName: string
+  picture: string
+  startDate: string
+  type: string
+  username: string
+  id: Number
+  courseTime: Date
+  lectureHall: string
+  studentId: number
+}
+
+export interface JoinCourse {
+  username: string
+  password: string
+  courseId: number
+}
+
+export interface Comments {
+  id: number
+  text: string
+  likes: string
+  username: string
+  studentId: number
+  courseId: number
+}
